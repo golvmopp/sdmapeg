900eaa07ce1e692a4f2ed972af0545e06c7a5f0e
rascal <raksooo@hotmail.com>
2013-03-29 12:58:51 +0100

    Created basic interfaces for the Common module

---
+++ project/common/src/main/java/se/sdmapeg/common/Connection.java
@@ -0,0 +1,13 @@
+package se.sdmapeg.common;
+
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetAddress;
+
+public interface Connection<S extends Message, R extends Message> extends Closeable {
+	InetAddress getAddress();
+	void send(S message);
+	R receive();
+	void close() throws IOException;
+	boolean isOpen();
+}
---
+++ project/common/src/main/java/se/sdmapeg/common/Task.java
@@ -0,0 +1,8 @@
+package se.sdmapeg.common;
+
+import java.io.Serializable;
+
+public interface Task<R> extends Serializable {
+	<T> T accept(TaskVisitor<T> visitor);
+	Class<R> resultType();
+}
