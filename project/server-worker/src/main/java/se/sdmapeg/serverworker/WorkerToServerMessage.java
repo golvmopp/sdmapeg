579deca848143970e5718708e8f905fe32df328b
golvmopp <github@golvmopp.it>
2013-03-29 17:29:50 +0100

    Written JavaDoc for server-worker module interfaces.

--- project/server-worker/src/main/java/se/sdmapeg/serverworker/WorkerToServerMessage.java
+++ project/server-worker/src/main/java/se/sdmapeg/serverworker/WorkerToServerMessage.java
@@ -2,9 +2,20 @@ package se.sdmapeg.serverworker;
 
 import se.sdmapeg.common.Message;
 
+/**
+ * A representation of a message from a Worker to the Server.
+ * 
+ */
 public interface WorkerToServerMessage extends Message {
-	<T> T accept(Visitor<T> visitor);
 
+	/**
+	 * Accept method for use with the visitor pattern.
+	 */
+    	<T> T accept(Visitor<T> visitor);
+
+    	/**
+	 * A visitor for messages from Worker to Server.
+	 */
 	interface Visitor<T> {
 
 	}
