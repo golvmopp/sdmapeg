3233f75a1222ed6c63e50c1d7ab02372093d71a4
Niclas Alexandersson <alniniclas@gmail.com>
2013-03-29 12:15:36 +0100

    Added a dummy class.

---
+++ project/common/src/main/java/se/sdmapeg/common/Dummy.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.sdmapeg.common;
+
+/**
+ *
+ * @author niclas
+ */
+public class Dummy {
+	
+}
