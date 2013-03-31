f250e2812a8bd44712e4a30d512d34638e34cb5b
golvmopp <github@golvmopp.it>
2013-03-31 16:02:48 +0200

    Added interface TaskPerformer.

---
+++ project/worker/src/main/java/se/sdmapeg/worker/TaskPerformer.java
@@ -0,0 +1,15 @@
+package se.sdmapeg.worker;
+
+import java.util.concurrent.ExecutionException;
+
+import se.sdmapeg.common.Task;
+
+/**
+ * Interface for classes capable of performing a particular task.
+ */
+public interface TaskPerformer<T extends Task<R>,R> {
+    /**
+     * Performs the specified task and returns its result.
+     */
+    R perform(T task) throws ExecutionException;
+}
