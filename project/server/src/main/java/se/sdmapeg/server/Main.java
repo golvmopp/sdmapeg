c6ad7d19d7301fe6696d5587e94fc5c2d099a92c
Niclas Alexandersson <alniniclas@gmail.com>
2013-04-17 11:24:14 +0200

    Created Main class for the server module.

---
+++ project/server/src/main/java/se/sdmapeg/server/Main.java
@@ -0,0 +1,31 @@
+package se.sdmapeg.server;
+
+import java.util.Scanner;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se.sdmapeg.common.communication.CommunicationException;
+
+/**
+ *
+ * @author niclas
+ */
+public class Main {
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+	public static void main(String[] args) {
+		Server server;
+		try {
+			server = new ServerImpl();
+		} catch (CommunicationException ex) {
+			LOG.error("Failed to create server", ex);
+			return;
+		}
+		server.start();
+		System.out.println("Server started and running. Type \"exit\" to close.");
+		Scanner scanner = new Scanner(System.in);
+		while (!scanner.nextLine().equals("exit")) {
+			// Do nothing
+		}
+		server.shutDown();
+	}
+}
