fe0b766cd4b92502beeacf7de0a0bdce051e3ab3
Niclas Alexandersson <alniniclas@gmail.com>
2013-04-17 07:16:58 +0200

    Made ConnectionHandlerImpl throw the right exception, and fixed the indentation.

--- project/server/src/main/java/se/sdmapeg/server/communication/ConnectionHandlerImpl.java
+++ project/server/src/main/java/se/sdmapeg/server/communication/ConnectionHandlerImpl.java
@@ -13,35 +13,37 @@ import se.sdmapeg.common.communication.ConnectionImpl;
  *
  */
 public final class ConnectionHandlerImpl implements ConnectionHandler {
+	private final ServerSocket serverSocket;
 
-    private final ServerSocket serverSocket;
-    
-    private ConnectionHandlerImpl(int port) throws CommunicationException{
-	try {
-	    serverSocket = new ServerSocket(port);
-	} catch (IOException e) {
-	    throw new CommunicationException(e);
+	private ConnectionHandlerImpl(int port) throws CommunicationException {
+		try {
+			serverSocket = new ServerSocket(port);
+		} catch (IOException e) {
+			throw new CommunicationException(e);
+		}
 	}
-    }
-    
-    
-    @Override
-    public void close() throws IOException {
-	serverSocket.close();	
-    }
 
-    @Override
-    public Connection accept() throws CommunicationException, SocketException {
-	return ConnectionImpl.newConnection(serverSocket.accept());
-	//TODO: Fix this whenever the exceptions here are sorted out. 
-    }
+	@Override
+	public void close() throws IOException {
+		serverSocket.close();
+	}
 
-    @Override
-    public boolean isOpen() {
-	return !serverSocket.isClosed();
-    }
-    
-    public static ConnectionHandler newConnectionHandler(int port) throws CommunicationException{
-	return new ConnectionHandlerImpl(port);	
-    }
+	@Override
+	public Connection accept() throws CommunicationException, SocketException {
+		try {
+			return ConnectionImpl.newConnection(serverSocket.accept());
+		} catch(IOException e) {
+			throw new CommunicationException(e);
+		}
+	}
+
+	@Override
+	public boolean isOpen() {
+		return !serverSocket.isClosed();
+	}
+
+	public static ConnectionHandler newConnectionHandler(int port) throws
+			CommunicationException {
+		return new ConnectionHandlerImpl(port);
+	}
 }
