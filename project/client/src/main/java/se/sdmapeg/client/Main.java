8b359aa96b9c2668a4159f157fdf78d1c46bbad9
Niclas Alexandersson <alniniclas@gmail.com>
2013-04-17 17:42:47 +0200

    Fixed imports in the client Main class.

--- project/client/src/main/java/se/sdmapeg/client/Main.java
+++ project/client/src/main/java/se/sdmapeg/client/Main.java
@@ -1,8 +1,6 @@
 package se.sdmapeg.client;
 
 import javax.swing.JOptionPane;
-
-import com.sun.deploy.trace.LoggerTraceListener;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.sdmapeg.client.gui.ClientView;
