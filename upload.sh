5470fdd1bd080a2e9751524e739d69fd90dac377
rascal <oskar@oskarnyberg.com>
2013-04-17 23:39:30 +0200

    Updated git ignore

--- upload.sh
+++ upload.sh
@@ -2,5 +2,4 @@
 cd ~/Programmering/sdmapeg/project
 mvn package
 cd server/target
-mv server-1.0-SNAPSHOT.jar server.jar
-scp server.jar Trivoc@95.80.13.184:./sdmapeg/
+scp server-1.0-SNAPSHOT.jar Trivoc@95.80.13.184:./sdmapeg/server.jar
