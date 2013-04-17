6d7654b9ebc28b66bad94a501e5f953cb53c5e30
rascal <oskar@oskarnyberg.com>
2013-04-17 12:56:10 +0200

    Added logging and error handling.

--- project/client/src/main/java/se/sdmapeg/client/Client.java
+++ project/client/src/main/java/se/sdmapeg/client/Client.java
@@ -1,7 +1,13 @@
 package se.sdmapeg.client;
 
+import se.sdmapeg.common.tasks.Task;
+import se.sdmapeg.serverclient.ClientTaskId;
+
 /**
  * Interface for representing the actual client.
  */
 public interface Client {
+	public ClientTaskId addTask(Task task);
+	public void sendTask(ClientTaskId id);
+	public void receive();
 }
--- project/client/src/main/java/se/sdmapeg/client/Main.java
+++ project/client/src/main/java/se/sdmapeg/client/Main.java
@@ -2,16 +2,26 @@ package se.sdmapeg.client;
 
 import javax.swing.JOptionPane;
 
+import com.sun.deploy.trace.LoggerTraceListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import se.sdmapeg.client.gui.ClientView;
 import se.sdmapeg.common.communication.CommunicationException;
 
 public class Main {
+	private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
 	public static void main(String[] args) {
-		String host = JOptionPane.showInputDialog("Adress:");
-		int port = Integer.parseInt(JOptionPane.showInputDialog("Port:"));
+		String host = JOptionPane.showInputDialog("Address:");
+		String portString = JOptionPane.showInputDialog("Port:");
+		while (!portString.matches("\\d+")) {
+			JOptionPane.showMessageDialog(null, "Invalid port");
+			portString = JOptionPane.showInputDialog("Port:");
+		}
+		int port = Integer.parseInt(portString);
 		ClientView view = ClientView.newView();
 		try {
-			ClientImpl.newClientImp(view, host, port);
+			ClientImpl.newClientImp(view, host, port).receive();
 		} catch (CommunicationException e) {
 			JOptionPane.showMessageDialog(null, "Could not connect to server.");
 			view.dispose();
