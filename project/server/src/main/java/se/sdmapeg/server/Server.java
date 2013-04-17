6b89b4ce962d35f70c1b2865a9b3425761a5418f
Niclas Alexandersson <alniniclas@gmail.com>
2013-04-17 10:52:16 +0200

    Updated the Server interface.

--- project/server/src/main/java/se/sdmapeg/server/Server.java
+++ project/server/src/main/java/se/sdmapeg/server/Server.java
@@ -4,4 +4,15 @@ package se.sdmapeg.server;
  * Interface for representing the actual server.
  */
 public interface Server {
+
+	/**
+	 * Shuts down this Server.
+	 */
+	void shutDown();
+
+	/**
+	 * Starts this server.
+	 */
+	void start();
+	
 }
