b8bf932df97d91993dfef289e9a7f3394e2d5ef2
Niclas Alexandersson <alniniclas@gmail.com>
2013-04-17 18:27:04 +0200

    Added code which starts and stops the worker.

--- project/worker/src/main/java/se/sdmapeg/worker/Main.java
+++ project/worker/src/main/java/se/sdmapeg/worker/Main.java
@@ -10,31 +10,30 @@ import se.sdmapeg.common.communication.CommunicationException;
 import se.sdmapeg.common.communication.ConnectionImpl;
 
 public class Main {
-
-    /**
-     * @param args
-     * @throws CommunicationException 
-     */
-    public static void main(String[] args) throws CommunicationException {
-	String host = JOptionPane.showInputDialog("Address:");
-	String portString = JOptionPane.showInputDialog("Port:");
-	Server server;
-	
-	
-	while (!portString.matches("\\d+")) {
-		JOptionPane.showMessageDialog(null, "Invalid port");
-		portString = JOptionPane.showInputDialog("Port:");
+	/**
+	 * @param args
+	 * @throws CommunicationException
+	 */
+	public static void main(String[] args) throws CommunicationException {
+		String host = JOptionPane.showInputDialog("Address:");
+		String portString = JOptionPane.showInputDialog("Port:");
+		Server server;
+		while (!portString.matches("\\d+")) {
+			JOptionPane.showMessageDialog(null, "Invalid port");
+			portString = JOptionPane.showInputDialog("Port:");
+		}
+		int port = Integer.parseInt(portString);
+		try {
+			server = ServerImpl.newServer(ConnectionImpl
+					.newConnection(new Socket(host, port)));
+		} catch (CommunicationException | IOException e) {
+			throw new CommunicationException();
+		}
+		int poolSize = Runtime.getRuntime().availableProcessors();
+		Worker worker = WorkerImpl.newWorkerImpl(poolSize, server,
+												 new TaskPerformerImpl());
+		worker.start();
+		JOptionPane.showMessageDialog(null, "Worker running...");
+		worker.stop();
 	}
-	int port = Integer.parseInt(portString);
-	try {
-	    server = ServerImpl.newServer(ConnectionImpl.newConnection
-		    (new Socket (host, port)));
-	} catch (CommunicationException|IOException e) {
-		throw new CommunicationException();
-	}
-	int poolSize = Runtime.getRuntime().availableProcessors();
-	Worker worker = WorkerImpl.newWorkerImpl(poolSize, server,
-		new TaskPerformerImpl());
-    }
-
 }
