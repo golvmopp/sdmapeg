0c13cbca0f45467948246b0dffa830973ce2a895
Victor Nilsson <trivoc92@gmail.com>
2013-04-22 00:27:39 +0200

    Worked on TaskPanel

--- project/client/src/main/java/se/sdmapeg/client/gui/TaskPanel.java
+++ project/client/src/main/java/se/sdmapeg/client/gui/TaskPanel.java
@@ -1,5 +1,8 @@
 package se.sdmapeg.client.gui;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.swing.JPanel;
@@ -10,13 +13,26 @@ public class TaskPanel extends JPanel {
 		CREATED, SENT, COMPLETED, FAILED;
 	}
 
-	Date timeStamp;
-	String typeName;
-	String name;
-	TaskState state;
+	private Calendar timeStamp;
+	private String typeName;
+	private String name;
+	private TaskState state;
 	
-	public TaskPanel(){
+	public TaskPanel(String typeName){
+		this.typeName = typeName;
+		this.timeStamp = Calendar.getInstance();
+		this.state = TaskState.CREATED;
+		this.setLayout(new BorderLayout());
+		
+		JPanel centerPanel = new JPanel(new GridLayout(1, 2));
+		JPanel centerPaneltext = new JPanel(new GridLayout(3, 1));
+		JPanel centerPanelCancel = new JPanel();
+		this.add(centerPanel, BorderLayout.CENTER);
 		
 	}
-
+	
+	public TaskPanel(String typeName, String name){
+		this(typeName);
+		this.name = name;
+	}
 }
