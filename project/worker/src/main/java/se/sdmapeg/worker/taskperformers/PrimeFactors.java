df4d1a551467a86fc799cab40a21cd168b42ab51
golvmopp <github@golvmopp.it>
2013-04-23 14:33:04 +0200

    Added testing method for prime factorization

--- project/worker/src/main/java/se/sdmapeg/worker/taskperformers/PrimeFactors.java
+++ project/worker/src/main/java/se/sdmapeg/worker/taskperformers/PrimeFactors.java
@@ -4,9 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public final class PrimeFactors {
+	public static void main(String args[]) {
+		long n = (long) Integer.MAX_VALUE * Integer.MAX_VALUE + 1L;
+		long start = System.nanoTime();
+		List<Long> result = findPrimeFactors(n);
+		long end = System.nanoTime();
+		System.out.println("The factors of " + n + " are " + result + " in " +(end-start)/1000000 + "ms");
+	}
+
 	public static List<Long> findPrimeFactors(long product) {
 		ArrayList<Long> primes = new ArrayList<>();
-		for (long i = 2; i * i < product; i++) {
+		for (long i = 2; product != 1; i++) {
 			while (product % i == 0) {
 				primes.add(Long.valueOf(i));
 				product /= i;
