a0b024820f095f05cb631e82ec6df4ffc9031883
rascal <oskar@oskarnyberg.com>
2013-04-24 13:32:34 +0200

    Continued implementation WorkerView

--- project/worker/src/main/java/se/sdmapeg/worker/gui/WorkerView.java
+++ project/worker/src/main/java/se/sdmapeg/worker/gui/WorkerView.java
@@ -1,17 +1,26 @@
 package se.sdmapeg.worker.gui;
 
+import se.sdmapeg.common.TimeFormatter;
 import se.sdmapeg.serverworker.TaskId;
 import se.sdmapeg.worker.Worker;
 import se.sdmapeg.worker.WorkerListener;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public final class WorkerView extends JFrame implements WorkerListener {
 	private final Worker worker;
 	private long startTime;
 	private int totalTasks = 0;
 	private int tasksPerformedCounter = 0;
+
+	private JLabel timer;
+	private JLabel tasksReceived;
+	private JLabel tasksPerformed;
+	private JLabel activeTasks;
+	private JLabel queueLength;
 
 	public WorkerView(Worker worker) {
 		this.worker = worker;
@@ -24,7 +33,7 @@ public final class WorkerView extends JFrame implements WorkerListener {
 		JPanel statistics = new JPanel(new GridLayout(0, 2));
 
 		JLabel timerLabel = new JLabel("Time since startup: ", SwingConstants.RIGHT);
-		JLabel timer = new JLabel("00:00:00");
+		timer = new JLabel("00:00:00");
 		statistics.add(timerLabel);
 		statistics.add(timer);
 
@@ -34,22 +43,22 @@ public final class WorkerView extends JFrame implements WorkerListener {
 		statistics.add(availableProcessors);
 
 		JLabel tasksReceivedLabel = new JLabel("Number of received tasks: ", SwingConstants.RIGHT);
-		JLabel tasksReceived = new JLabel(Integer.toString(totalTasks));
+		tasksReceived = new JLabel(Integer.toString(totalTasks));
 		statistics.add(tasksReceivedLabel);
 		statistics.add(tasksReceived);
 
 		JLabel tasksPerformedLabel = new JLabel("Number of performed tasks: ", SwingConstants.RIGHT);
-		JLabel tasksPerformed = new JLabel(Integer.toString(tasksPerformedCounter));
+		tasksPerformed = new JLabel(Integer.toString(tasksPerformedCounter));
 		statistics.add(tasksPerformedLabel);
 		statistics.add(tasksPerformed);
 
 		JLabel activeTasksLabel = new JLabel("Number of tasks running: ", SwingConstants.RIGHT);
-		JLabel activeTasks = new JLabel(Integer.toString(Math.min(totalTasks - tasksPerformedCounter, Runtime.getRuntime().availableProcessors())));
+		activeTasks = new JLabel(Integer.toString(Math.min(totalTasks - tasksPerformedCounter, Runtime.getRuntime().availableProcessors())));
 		statistics.add(activeTasksLabel);
 		statistics.add(activeTasks);
 
 		JLabel queueLengthLabel = new JLabel("Queue length: ", SwingConstants.RIGHT);
-		JLabel queueLength = new JLabel(Integer.toString(totalTasks - tasksPerformedCounter));
+		queueLength = new JLabel(Integer.toString(totalTasks - tasksPerformedCounter));
 		statistics.add(queueLengthLabel);
 		statistics.add(queueLength);
 
@@ -60,6 +69,25 @@ public final class WorkerView extends JFrame implements WorkerListener {
 
 		pack();
 		setVisible(true);
+
+		new Timer(1000, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				updateTimer();
+			}
+		}).start();
+	}
+
+	private void updateTimer() {
+		TimeFormatter timeFormatter = new TimeFormatter(startTime);
+		timer.setText(timeFormatter.getFormattedHours() + ":" + timeFormatter.getFormattedMinutes() + ":" + timeFormatter.getFormattedSeconds());
+	}
+
+	private void updateStatistics() {
+		tasksReceived.setText(Integer.toString(totalTasks));
+		tasksPerformed.setText(Integer.toString(tasksPerformedCounter));
+		activeTasks.setText(Integer.toString(Math.min(totalTasks - tasksPerformedCounter, Runtime.getRuntime().availableProcessors())));
+		queueLength.setText(Integer.toString(totalTasks - tasksPerformedCounter));
 	}
 
 	@Override
@@ -70,6 +98,7 @@ public final class WorkerView extends JFrame implements WorkerListener {
 	@Override
 	public void taskAdded(TaskId taskId) {
 		totalTasks++;
+		updateStatistics();
 	}
 
 	@Override
@@ -80,6 +109,7 @@ public final class WorkerView extends JFrame implements WorkerListener {
 	@Override
 	public void taskFinished(TaskId taskId) {
 		tasksPerformedCounter++;
+		updateStatistics();
 	}
 
 	public static void main(String[] args) {
