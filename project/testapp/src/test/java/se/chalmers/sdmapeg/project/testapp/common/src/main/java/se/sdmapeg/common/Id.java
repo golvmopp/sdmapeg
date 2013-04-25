474150006c3fb78ae35f6c59b99bd2d6d60c039f
golvmopp <github@golvmopp.it>
2013-04-25 13:57:35 +0200

    Removed 'Testapp' project, don't need dat shit no more.

--- project/testapp/src/test/java/se/chalmers/sdmapeg/project/testapp/common/src/main/java/se/sdmapeg/common/Id.java
+++
@@ -1,9 +0,0 @@
-package se.sdmapeg.common;
-
-import java.io.Serializable;
-
-public interface Id extends Serializable {
-	boolean equals(Object o);
-
-	int hashCode();
-}
--- project/testapp/src/test/java/se/chalmers/sdmapeg/project/testapp/common/src/main/java/se/sdmapeg/common/IdGenerator.java
+++
@@ -1,5 +0,0 @@
-package se.sdmapeg.common;
-
-public interface IdGenerator<I extends Id> {
-	I newId();
-}
