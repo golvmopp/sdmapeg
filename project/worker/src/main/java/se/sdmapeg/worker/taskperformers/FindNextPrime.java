e044a606922ec41a4083c21ec4a9391a9ca7bf95
William Dahlberg <github@golvmopp.it>
2013-05-02 13:30:06 +0200

    New task: FindNextPrime

---
+++ project/worker/src/main/java/se/sdmapeg/worker/taskperformers/FindNextPrime.java
@@ -0,0 +1,34 @@
+package se.sdmapeg.worker.taskperformers;
+
+public class FindNextPrime {
+	/**
+	 * @param prime A prime number
+	 * @return given long if it isn't prime, next prime if prime is prime
+	 */
+	public static long findNextPrime(long prime) {
+		if (!isPrime(prime)) {
+			return prime;
+		}
+		
+		return nextPrime(prime);
+	}
+
+	private static long nextPrime(long prime) {
+		while (!isPrime(prime)) {
+			prime++;
+		}
+		return prime;
+	}
+
+	/**
+	 * @return true if prime.
+	 */
+	private static boolean isPrime(long prime) {
+		for (long i = 2; i < Math.sqrt(prime); i++) {
+			if (prime % i == 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
