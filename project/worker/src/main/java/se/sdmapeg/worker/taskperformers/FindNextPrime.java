c538b3de9f2365e9e116180840e46f54261ff262
golvmopp <github@golvmopp.it>
2013-05-13 19:16:11 +0200

    Added testing for FindNextPrimeTask

--- project/worker/src/main/java/se/sdmapeg/worker/taskperformers/FindNextPrime.java
+++ project/worker/src/main/java/se/sdmapeg/worker/taskperformers/FindNextPrime.java
@@ -5,14 +5,15 @@ import java.util.concurrent.ExecutionException;
 public final class FindNextPrime {
 	/**
 	 * @param prime A prime number
-	 * @return given long if it isn't prime, next prime if prime is prime
+	 * @return next prime if prime is prime
+	 * @throws ExecutionException if prime isn't prime, or not greater than 0.
 	 */
 	public static long findNextPrime(long prime) throws ExecutionException {
 		if (prime <= 0) {
 			throw new ExecutionException(new ArithmeticException("Starting prime must be higher than 0"));
 		}
 		if (!isPrime(prime)) {
-			return prime;
+			throw new ExecutionException(new ArithmeticException("Given long wasn't prime."));
 		}
 		return nextPrime(prime);
 	}
--- project/worker/src/test/java/se/sdmapeg/worker/taskperformers/FindNextPrimeTest.java
+++ project/worker/src/test/java/se/sdmapeg/worker/taskperformers/FindNextPrimeTest.java
@@ -2,13 +2,31 @@ package se.sdmapeg.worker.taskperformers;
 
 import static org.junit.Assert.*;
 
+import java.util.concurrent.ExecutionException;
+
 import org.junit.Test;
 
 public class FindNextPrimeTest {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testFindNextPrime() throws ExecutionException {
+		long input = 2147483629;
+		long expected = Integer.MAX_VALUE;
+		assertEquals(3, FindNextPrime.findNextPrime(2));
+		assertEquals(43, FindNextPrime.findNextPrime(41));
+		assertEquals(104729, FindNextPrime.findNextPrime(104723));
+		assertEquals(expected, FindNextPrime.findNextPrime(input));
+		
+	}
+	
+	@Test(expected=ExecutionException.class)
+	public void testFindNextPrimeLowerBound() throws Exception {
+		FindNextPrime.findNextPrime(0);
+	}
+	
+	@Test(expected=ExecutionException.class)
+	public void testFindNextPrimeNoPrime() throws Exception {
+		FindNextPrime.findNextPrime(120);
 	}
 
 }
