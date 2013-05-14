3c9f0e080498008e5d36d32b5b1da0f7f8e18334
rascal <oskar@oskarnyberg.com>
2013-05-14 08:31:13 +0200

    Implemented GUI in PythonTask

--- project/client/src/main/java/se/sdmapeg/client/gui/tasks/PythonTask/PythonTask.java
+++ project/client/src/main/java/se/sdmapeg/client/gui/tasks/PythonTask/PythonTask.java
@@ -1,15 +1,33 @@
 package se.sdmapeg.client.gui.tasks.PythonTask;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-/**
- * Created with IntelliJ IDEA.
- * User: Oskar
- * Date: 2013-05-13
- * Time: 11:28
- * To change this template use File | Settings | File Templates.
- */
 public class PythonTask extends JPanel {
+
+	public PythonTask() {
+		setLayout(new GridLayout(2, 1, 30, 10));
+
+		JButton write = new JButton("Create");
+		write.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//To change body of implemented methods use File | Settings | File Templates.
+			}
+		});
+		add(write);
+
+		JButton load = new JButton("Load");
+		load.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				//To change body of implemented methods use File | Settings | File Templates.
+			}
+		});
+		add(load);
+	}
 
 	public static void main(String[] args) {
 		JFrame frame = new JFrame("Hurr Durr");
