dfbe121003ebf31b7a6964d720aa4d71802d66ab
golvmopp <github@golvmopp.it>
2013-05-20 13:33:38 +0200

    Now testing everything in TaskPerformerImpl

--- project/worker/src/test/java/se/sdmapeg/worker/TaskPerformerImplTest.java
+++ project/worker/src/test/java/se/sdmapeg/worker/TaskPerformerImplTest.java
@@ -2,12 +2,16 @@ package se.sdmapeg.worker;
 
 import static org.junit.Assert.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 import org.junit.Test;
 
 import se.sdmapeg.common.tasks.FindNextIntTask;
 import se.sdmapeg.common.tasks.FindNextPrimeTask;
+import se.sdmapeg.common.tasks.PrimeFactorsTask;
+import se.sdmapeg.common.tasks.PythonTask;
 import se.sdmapeg.common.tasks.Result;
 
 public class TaskPerformerImplTest {
@@ -67,6 +71,58 @@ public class TaskPerformerImplTest {
 		FindNextPrimeTask testTask = FindNextPrimeTask.newFindNextPrimeTask(-12);
 		TaskPerformerImpl instance = new TaskPerformerImpl();
 		Result<Long> result = instance.performFindNextPrimeTask(testTask);
+		try {
+			result.get();
+			fail();
+		} catch (ExecutionException ex) {
+			// Success!
+		}
+	}
+	
+	@Test
+	public void testPerformPythonTask() {
+		String pythonCode = "result=2";
+		PythonTask testTask = PythonTask.newPythonTask(pythonCode);
+		TaskPerformerImpl instance = new TaskPerformerImpl();
+		Result<String> result = instance.performPythonTask(testTask);
+		try {
+			assertEquals("2", result.get());
+		} catch (ExecutionException ex) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testPerformPythonTaskSyntaxError() {
+		String pythonCode = "result = ";
+		PythonTask testTask = PythonTask.newPythonTask(pythonCode);
+		TaskPerformerImpl instance = new TaskPerformerImpl();
+		Result<String> result = instance.performPythonTask(testTask);
+		try {
+			result.get();
+			fail();
+		} catch (ExecutionException ex) {
+			// Success!
+		}
+	}
+	
+	@Test
+	public void testFindPrimeFactors() {
+		PrimeFactorsTask testTask = PrimeFactorsTask.newPrimeFactorTask(10);
+		TaskPerformerImpl instance = new TaskPerformerImpl();
+		Result<List<Long>> result = instance.findPrimeFactors(testTask);
+		try {
+			assertEquals(Arrays.asList(2L,5L), result.get());
+		} catch (ExecutionException ex) {
+			fail();
+		}
+	}
+	
+	@Test
+	public void testFindPrimeFactorsLowerBound() {
+		PrimeFactorsTask testTask = PrimeFactorsTask.newPrimeFactorTask(-12);
+		TaskPerformerImpl instance = new TaskPerformerImpl();
+		Result<List<Long>> result = instance.findPrimeFactors(testTask);
 		try {
 			result.get();
 			fail();
