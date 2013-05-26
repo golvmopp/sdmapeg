ee3003228c6d71f82ce70e92b71f6fd3ee6c195a
Victor Nilsson <trivoc92@gmail.com>
2013-05-26 15:41:42 +0200

    Added javadoc for taskperformers

--- project/worker/src/main/java/se/sdmapeg/worker/taskperformers/FindNextInteger.java
+++ project/worker/src/main/java/se/sdmapeg/worker/taskperformers/FindNextInteger.java
@@ -2,7 +2,20 @@ package se.sdmapeg.worker.taskperformers;
 
 import java.util.concurrent.ExecutionException;
 
+/**
+ * A simple task for finding the next integer, given an integer to start from. 
+ * 
+ * @author Golvmopp
+ *
+ */
 public class FindNextInteger {
+	/**
+	 * 
+	 * 
+	 * @param The value to increment. 
+	 * @return The incremented value.
+	 * @throws ExecutionException If the start value equals the maximum Integer value. 
+	 */
 	public static int findNextInteger(int start) throws ExecutionException {
 		if (start==Integer.MAX_VALUE) {
 			throw new ExecutionException(new ArithmeticException("Integer overflow"));
--- project/worker/src/main/java/se/sdmapeg/worker/taskperformers/PrimeFactors.java
+++ project/worker/src/main/java/se/sdmapeg/worker/taskperformers/PrimeFactors.java
@@ -4,11 +4,23 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+/**
+ * A task finding the prime factors of a given number. 
+ * @author Golvmopp
+ *
+ */
 public final class PrimeFactors {
+	
+	/**
+	 * 
+	 * @param The number to factor
+	 * @return A list of the prime factors of the parameter. 
+	 * @throws ExecutionException Whenever the input is less than 2. 
+	 */
 	public static List<Long> findPrimeFactors(long number) throws ExecutionException {
 		if (number<2) {
 			throw new ExecutionException(new IllegalArgumentException("The number" +
-					" to factor must be equal to or grater than two (number >= 2)."));
+					" to factor must be equal to or greater than two (number >= 2)."));
 		}
 		ArrayList<Long> primes = new ArrayList<>();
 		for (long i = 2; number != 1; i++) {
